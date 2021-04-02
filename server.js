851412aff460a77a6065a577888c63d1a8303578
kcmodev <shchristensen3@gmail.com>
2021-04-02 11:24:30 -0500

    added reoutes and db connection stuff

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/', (req, res) => { res.send('API Running...')});
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
