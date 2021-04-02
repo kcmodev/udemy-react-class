042ebe5788eded5ba0b52dc7009c9aa337c21ae5
kcmodev <shchristensen3@gmail.com>
2021-04-02 17:01:00 -0500

    added jwt support and user database table

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
+const User = require('../../models/User');
 
 /**
- * @route GET api/users
- * @desc Test route
+ * @route POST api/users
+ * @desc Register user
  * @access Public
  */
-router.get('/', (req, res) => {
-    res.send('User route')
-})
+router.post(
+    '/',
+    [
+        check('name', 'Name is required.').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password between 6 and 30 characters.'
+        ).isLength({ min: 6, max: 30 }),
+    ],
+
+    // Handle request and response
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.mapped() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            // Check if user exists
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(500).json({
+                    errors: { msg: 'User already exists.' },
+                });
+            }
+
+            // Get user's Gravatar
+            const avatar = gravatar.url(email, {
+                // size
+                s: '200',
+                // rating
+                r: 'pg',
+                // default avatar image
+                d: 'mm',
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+            });
+
+            // Encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt); // Hash the password
+            await user.save();
+
+            // Return JSON Webtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.log(`Error validating user: ${err.message}`);
+            res.status(500).send('Server error.');
+        }
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => { res.send('API Running...')});
+// Init middleware
+app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/auth', require('./routes/api/auth'));
+// GET request to index
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
